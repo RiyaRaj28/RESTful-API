83e944d3c25577e833167a01c4ace474b3ebb262
RiyaRaj28 <rrajj7674@gmail.com>
2024-04-25 01:58:47 +0530

    updated

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -8,10 +8,10 @@ const studentRouter = require("./routers/student");
 const app = express();
 const port = process.env.PORT || 3000; 
 
-app.get("/", (req, res) =>
-{
-    res.send("hello from the other side by riya.");
-});
+// app.get("/", (req, res) =>
+// {
+//     res.send("hello from the other side by riya.");
+// });
 
 app.use(express.json());
 app.use(studentRouter);
@@ -31,97 +31,17 @@ app.use(studentRouter);
 //     // res.send("Hello from the other side");
 // });
 
-//create new student using async await functions
-app.post("/students", async(req, res)=> 
-{
-    try
-    {
-    const user = new Student(req.body);
-    const createUser = await user.save();
-    res.status(201).send(createUser);
-    }
-    catch(e) 
-    {  
-        res.status(400).send(e);
-    }
-})
 
-
-//to read the data of the registered students
-app.get("/students", async(req, res) => {
-    try{
-        const studentsData = await Student.find(); 
-        res.send(studentsData);
-    }catch{
-        res.send(e);
-    }
-})
-
-
-
-
-app.get("/students/:id", async(req, res)=> {
-    try
-    {
-        const _id = req.params.id;
-        // console.log(req.params.id);  
-        // res.send(req.params.id); 
-        //fetch all data by the id of the student 
-
-        const studentData = await Student.findById(_id);
-        console.log(studentData);
-
-
-        //case where the user enters a non existent data
-        if(!studentData)
-        {
-            return res.status(404).send();
-        }
-        else
-        {
-            res.send(studentData); 
-        }
-    }
-
-    catch(e)
-    {
-        res.send(e);
-    }
-})
-
-
-//to delete 
-app.delete("/students/id:", async(req,res) =>
-
-{
-   
-    
-        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
-        if(!req.params.id)
-        {
-            return res.status(404).send();
-        }
-        else
-        { 
-
-        }
-    
-
-
-})
-
-
-
-app.listen(port, () => 
+router.listen(port, () => 
 {
     console.log(`connection is setup at ${port}`); 
 });
 
 
-// You do not need express.json() and express.urlencoded()
-// for GET requests or DELETE requests.
-//  We only need it for post and put request.
+// // You do not need express.json() and express.urlencoded()
+// // for GET requests or DELETE requests.
+// //  We only need it for post and put request.
 
-//  express.json() is a method inbuilt in express to recognize the incoming
-//  request object as a JSON object. This method is called as a middleware 
-//  in your application using the code: app.use(express.json());
+// //  express.json() is a method inbuilt in express to recognize the incoming
+// //  request object as a JSON object. This method is called as a middleware 
+// //  in your application using the code: app.use(express.json());
--- restfulapi/src/routers/student.js
+++ restfulapi/src/routers/student.js
@@ -3,8 +3,83 @@ const router = new express.Router();
 
 //define the router
 
-router.get("/thapa", (req,res)=> {
-    res.send("Hello whatsUp guys"); 
-}); 
+// router.get("/", (req,res)=> {
+//     res.send("Hello whatsUp guys"); 
+// }); 
+
+// create new student using async await functions
+router.post("/students", async(req, res)=> 
+{
+    try
+    {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+    }
+    catch(e) 
+    {  
+        res.status(400).send(e);
+    }
+})
+
+
+//to read the data of the registered students
+router.get("/students", async(req, res) => {
+    try{
+        const studentsData = await Student.find(); 
+        res.send(studentsData);
+    }catch{
+        res.send(e);
+    }
+})
+
+
+
+
+router.get("/students/:id", async(req, res)=> {
+    try
+    {
+        const _id = req.params.id;
+        // console.log(req.params.id);  
+        // res.send(req.params.id); 
+        //fetch all data by the id of the student 
+
+        const studentData = await Student.findById(_id);
+        console.log(studentData);
+
+
+        //case where the user enters a non existent data
+        if(!studentData)
+        {
+            return res.status(404).send();
+        }
+        else
+        {
+            res.send(studentData); 
+        }
+    }
+
+    catch(e)
+    {
+        res.send(e);
+    }
+})
+
+
+//to delete 
+router.delete("/students/id:", async(req,res) =>{
+
+try{  
+        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+        if(!req.params.id)
+        {
+            return res.status(404).send();
+        }
+        res.send(deleteStudent); 
+    }catch(e){
+        res.status(500).send(e);
+    }
+    
+})
 
 module.exports = router;
